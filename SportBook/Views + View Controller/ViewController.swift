037f90636779400b821ed13600a3f2490fc9fd0f
Matthew Jagiela <>
2021-04-24 11:51:13 -0400

    feat: homescreen basic layout

--- SportBook/Views + View Controller/ViewController.swift
+++ SportBook/Views + View Controller/ViewController.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
-import Combine
+
+protocol HomeViewControllerDelegate {
+    func showLoading()
+    func presentDetails()
+    func presentAPIError(error: APIError)
+}
 
 class ViewController: UIViewController {
     
-    var subscriptions: Set<AnyCancellable> = []
+    let loadingAlert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
     
     let handler = APIRequest()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        handler.getSportResults().receive(on: RunLoop.main).sink { completion in
-            
-        } receiveValue: { [weak self] info in
-            
-        }.store(in: &subscriptions)
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    @IBAction func fetchRecords(_ sender: Any) {
+        //TODO: Call View Model to handle fetching the records
+    }
+    
+}
 
+extension ViewController: HomeViewControllerDelegate {
+    func presentAPIError(error: APIError) {
+        
+    }
+    
+    func showLoading() {
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating();
+
+        loadingAlert.view.addSubview(loadingIndicator)
+        present(loadingAlert, animated: true, completion: nil)
+    }
+    
+    func presentDetails() {
+        //TODO: present detail controller
+    }
+    
+    
 }
 
