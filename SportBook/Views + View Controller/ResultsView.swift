3121287282ba66ca84c8af810736356410583ad9
Matthew Jagiela <matthewjagiela@icloud.com>
2021-04-24 20:00:19 -0400

    trying to do this with SUI

---
+++ SportBook/Views + View Controller/ResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsView.swift
+//  SportBook
+//
+//  Created by Matthew Jagiela on 4/24/21.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultsView()
+    }
+}
---
+++ SportBook/Views + View Controller/ResultsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultsViewController.swift
+//  SportBook
+//
+//  Created by Matthew Jagiela on 4/24/21.
+//
+
+import UIKit
+import SwiftUI
+
+class ResultsViewController: UIViewController {
+    
+    var hostingController: UIHostingController<ResultsView>?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
